c9be3fcd9a479dd2f093d74aafb38add43421b8d
André Fajardo <117539643+andrefajardo@users.noreply.github.com>
2023-04-03 17:15:55 -0300

    Exercício M1S08Ex-02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ButtonBoxComponent } from './components/button-box/button-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    ButtonBoxComponent
   ],
   imports: [
     BrowserModule
